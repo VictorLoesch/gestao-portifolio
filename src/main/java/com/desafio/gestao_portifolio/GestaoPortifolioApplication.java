24b009bdd03e3813d9dbea577a6ea101bfaa3418
VictorLoesch <victorsuto10@hotmail.com>
2025-07-26 01:01:49 -0300

    Commit Inicial

---
+++ src/main/java/com/desafio/gestao_portifolio/GestaoPortifolioApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.gestao_portifolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoPortifolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoPortifolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/gestao_portifolio/GestaoPortifolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.gestao_portifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoPortifolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
